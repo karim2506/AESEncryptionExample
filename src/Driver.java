f531646b2410295baf2c82a3f499e1703fb57f89
karim2506 <kbenh016@uottawa.ca>
2018-09-20 14:16:22 -0400

    added symmetric key encryption

--- src/Driver.java
+++ src/Driver.java
@@ -27,11 +27,12 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
 public class Driver {
-	
+
 	static String inputFileDirectory = "src\\fileToEncrypt.txt";
 	static String encryptedFileDirectory = "src\\encryptedText.txt";
 	static String decryptedFileDirectory = "src\\decryptedText.txt";
 	static String hashedFileDirectory = "src\\hashedText.txt";
+	static String keyFileDirectory = "src\\symmetricKey.txt";
 
 	private static void fileProcessor(int cipherMode, String key, File inputFile, File outputFile) throws Exception {
 		Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
@@ -50,60 +51,59 @@ public class Driver {
 		inputStream.close();
 		outputStream.close();
 	}
-	
+
 	private static void printContent(String filePath) {
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-			   String line = null;
-			   while ((line = br.readLine()) != null) {
-			       System.out.println(line);
-			   }
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
 			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
-	
+
 	private static String hashFile(String filePath) throws Exception {
 		MessageDigest md = MessageDigest.getInstance("MD5");
-	    md.update(Files.readAllBytes(Paths.get(filePath)));
-	    byte[] digest = md.digest();
-	    
-	    String hash = DatatypeConverter
-	      .printHexBinary(digest).toUpperCase();
-	    
-	    FileOutputStream outputStream = new FileOutputStream(hashedFileDirectory);
+		md.update(Files.readAllBytes(Paths.get(filePath)));
+		byte[] digest = md.digest();
+
+		String hash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+		FileOutputStream outputStream = new FileOutputStream(hashedFileDirectory);
 		outputStream.write(hash.getBytes());
 		outputStream.close();
-	    
-	    return hash;
+
+		return hash;
 	}
-	
+
 	public static String sign(String plainText, PrivateKey privateKey) throws Exception {
-	    Signature privateSignature = Signature.getInstance("SHA256withRSA");
-	    privateSignature.initSign(privateKey);
-	    privateSignature.update(plainText.getBytes());
+		Signature privateSignature = Signature.getInstance("SHA256withRSA");
+		privateSignature.initSign(privateKey);
+		privateSignature.update(plainText.getBytes());
 
-	    byte[] signature = privateSignature.sign();
+		byte[] signature = privateSignature.sign();
 
-	    return Base64.getEncoder().encodeToString(signature);
+		return Base64.getEncoder().encodeToString(signature);
 	}
-	
+
 	private static KeyPair generateKeyPair() throws Exception {
-	    KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-	    generator.initialize(2048, new SecureRandom());
-	    KeyPair pair = generator.generateKeyPair();
+		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+		generator.initialize(2048, new SecureRandom());
+		KeyPair pair = generator.generateKeyPair();
 
-	    return pair;
+		return pair;
 	}
-	
+
 	private static boolean verify(String plainText, String signature, PublicKey publicKey) throws Exception {
-	    Signature publicSignature = Signature.getInstance("SHA256withRSA");
-	    publicSignature.initVerify(publicKey);
-	    publicSignature.update(plainText.getBytes());
+		Signature publicSignature = Signature.getInstance("SHA256withRSA");
+		publicSignature.initVerify(publicKey);
+		publicSignature.update(plainText.getBytes());
 
-	    byte[] signatureBytes = Base64.getDecoder().decode(signature);
+		byte[] signatureBytes = Base64.getDecoder().decode(signature);
 
-	    return publicSignature.verify(signatureBytes);
+		return publicSignature.verify(signatureBytes);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -112,25 +112,46 @@ public class Driver {
 		File encryptedFile = new File(encryptedFileDirectory);
 		File decryptedFile = new File(decryptedFileDirectory);
 		File hashedFile = new File(hashedFileDirectory);
-		
-//		KeyPair pair = generateKeyPair();
-//		String signature = sign("foobar", pair.getPrivate());
-//		//Let's check the signature
-//		boolean isCorrect = verify("foobar", signature, pair.getPublic());
-//		System.out.println("Signature correct: " + isCorrect);
+		File symmetricKeyFile = new File(keyFileDirectory);
+
+		// KeyPair pair = generateKeyPair();
+		// String signature = sign("foobar", pair.getPrivate());
+		// //Let's check the signature
+		// boolean isCorrect = verify("foobar", signature, pair.getPublic());
+		// System.out.println("Signature correct: " + isCorrect);
 
 		System.out.println("The initial file to Encrypt:");
 		printContent(inputFileDirectory);
-		
+
+		System.out.println();
+
 		System.out.println("The hash of the file:");
 		System.out.println(hashFile(inputFileDirectory));
-		
+
+		System.out.println();
+
 		Driver.fileProcessor(Cipher.ENCRYPT_MODE, key, hashedFile, encryptedFile);
 		System.out.println("The encrypted file:");
 		printContent(encryptedFileDirectory);
-		
+
+		System.out.println();
+
+		Driver.fileProcessor(Cipher.ENCRYPT_MODE, key, symmetricKeyFile, symmetricKeyFile);
+		System.out.println("The encrypted key file:");
+		printContent(keyFileDirectory);
+
+		System.out.println();
+
+		Driver.fileProcessor(Cipher.DECRYPT_MODE, key, symmetricKeyFile, symmetricKeyFile);
+		System.out.println("The decrypted key file:");
+		printContent(keyFileDirectory);
+
+		System.out.println();
+
 		Driver.fileProcessor(Cipher.DECRYPT_MODE, key, encryptedFile, decryptedFile);
-		System.out.println("The decrypted file:");
+		System.out.println("The decrypted hash file:");
 		printContent(decryptedFileDirectory);
+
+		System.out.println();
 	}
 }
